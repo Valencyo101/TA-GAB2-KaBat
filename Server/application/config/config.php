78d484c15a65c583b063039d59b2de7c67617e31
Valencyo101 <valencyoxtkj002@gmail.com>
2023-01-25 22:09:48 +0700

    vdhida: tambah beberapa file di folder asstes/css

--- Server/application/config/config.php
+++ Server/application/config/config.php
@@ -1,7 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-date_default_timezone_set("Asia/Jakarta");
-
 
 /*
 |--------------------------------------------------------------------------
@@ -25,7 +23,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/TA-GAB2-KaBat/';
 
 /*
 |--------------------------------------------------------------------------
